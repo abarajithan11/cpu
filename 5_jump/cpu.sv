6a2aba1d51c1c6fd700b3d6d0719eb33b026f537
Abarajithan G <abarajithan07@gmail.com>
2026-06-23 18:35:14 -0700

    Refactor cpu

--- 5_jump/cpu.sv
+++ 5_jump/cpu.sv
@@ -1,31 +1,26 @@
 `timescale 1ns/1ps
 
 module cpu (
-  input  logic        clk,
-  input  logic        reset,
-
-  output logic [7:0]  imem_addr,
-  input  logic [15:0] imem_rdata,
-
-  output logic [7:0]  dmem_addr,
-  input  logic [15:0] dmem_rdata,
+  input  logic        clk, reset,
+  output logic [7 :0] imem_addr,  dmem_addr,
+  input  logic [15:0] imem_rdata, dmem_rdata,
   output logic [15:0] dmem_wdata,
   output logic        dmem_wen
 );
   logic [7:0] pc, addr;
-  enum logic [3:0] {LOAD, STORE, MOVE, ADD, SUB, MUL, JNZ} op;
+  enum logic [3:0] {LOAD, STORE, MOVE, ADD, SUB, MUL, JNZ} opcode;
   logic [ 3:0] i_rs1, i_rs2, i_rd, i_reg;
   logic [15:0] regs [16];
   logic [15:0] reg_1, reg_2;
 
   always_comb begin
-    imem_addr                 = pc;
-    {addr        , i_reg, op} = imem_rdata;
-    {i_rs2, i_rs1, i_rd , op} = imem_rdata;
+    imem_addr = pc;
+    {addr        , i_reg, opcode} = imem_rdata;
+    {i_rs2, i_rs1, i_rd , opcode} = imem_rdata;
 
     dmem_addr  = addr;
     dmem_wdata = regs[i_reg];
-    dmem_wen   = !reset && op == STORE;
+    dmem_wen   = !reset && opcode == STORE;
 
     reg_1      = regs[i_rs1];
     reg_2      = regs[i_rs2];
@@ -38,13 +33,13 @@ module cpu (
     end else begin
       pc   <= pc + 1'b1;
 
-      case (op)
+      case (opcode)
         LOAD: regs[i_reg] <= dmem_rdata;
         MOVE: regs[i_rd ] <= reg_1;
         ADD : regs[i_rd ] <= reg_1 + reg_2;
         SUB : regs[i_rd ] <= reg_1 - reg_2;
         MUL : regs[i_rd ] <= reg_1 * reg_2;
-        JNZ : if (regs[i_reg] != '0) pc <= addr; // --- new
+        JNZ : if (regs[i_reg] != '0) pc <= addr; // --- new, last assignment wins
         default: ;
       endcase
     end
